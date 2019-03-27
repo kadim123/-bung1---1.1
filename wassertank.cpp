b68e3d5f05959f73b81df096743801de122bdd7c
Childish Flambino <48804180+kadim123@users.noreply.github.com>
2019-03-27 11:45:21 +0100

    Create wassertank.cpp

---
+++ wassertank.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <conio.h>
+
+#include "wassertank.h"
+
+
+void Wassertank::setGroesse(int menge) {
+	größe = menge;
+}
+
+void Wassertank::ausleeren() {
+	füllstand = 0;
+}
+
+void Wassertank::fuellen(int menge) {
+	if ((füllstand + menge) >= größe)
+		füllstand = größe;
+	else
+		füllstand += menge;
+
+}
+
+int Wassertank::getFuelstand() {
+	return füllstand;
+}
+
